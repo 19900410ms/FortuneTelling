f64f62d1e9b70f42b7d960790a3e273499e5e6cc
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-04-29 15:34:22 +0900

    ログイン・新規登録

--- FortuneTelling/Controller/LoginController.swift
+++ FortuneTelling/Controller/LoginController.swift
@@ -1,14 +1,34 @@
 import UIKit
+import Firebase
 
 class LoginController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func login(_ sender: Any) {
+        
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            
+            if error != nil {
+                
+                print(error as Any)
+                
+            } else {
+                
+                print("login！")
+                
+                self.performSegue(withIdentifier: "first", sender: nil)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
